c6a57e098f6de843b4f3a1bff765cdb3523e1fe2
bking1989 <bradley_king@me.com>
2018-10-30 18:42:53 -0700

    Did more work on the stylesheet and script

--- assets/javascript/game_test.js
+++ assets/javascript/game_test.js
@@ -15,18 +15,16 @@ var usedLetters = document.getElementById("usedLetters");
 // Possible options, and the process of a random one being selected 
 
 var possibleWords = [
-    "Smells Like Teen Spirit",
-    "Creep",
-    "Black Hole Sun",
-    "Loser",
-    "What I Got"];
+    "Nirvana",
+    "Sound Garden",
+    "Alice In Chains",
+    "Stone Temple Pilots",
+    "The Smashing Pumpkins"
+];
 var randomNumber = Math.floor(Math.random() * 5);
 var chosenWord = possibleWords[randomNumber];
 
 // Convert our string into an array, and make a censored version
-
-var answerArray = chosenWord.split("");
-var censoredWord = answerArray;
 
 // The actual game functions will start here
 
@@ -38,7 +36,6 @@ window.onload = function(e) {
             
             openingText.classList.add("hide");
             gameBoard.classList.remove("hide");
-            gameBoard.innerHTML = censoredWord.toString();
             totalWins.classList.remove("hide");
             totalWins.innerHTML = "Wins: " + winCounter;
             totalLosses.classList.remove("hide");
@@ -48,18 +45,7 @@ window.onload = function(e) {
             usedLetters.classList.remove("hide");
 
         } else if (openingText.classList.contains("hide")) {
-            
-            // After opening screen is cleared, game begins
-            // The first step is recording the user's input as a variable
 
-            var userChoice = event.key.toLocaleLowerCase();
-
-            //Then, we start applying the user's entry to the hidden word
-
-            if (answerArray.indexOf(userChoice) >= 0) {
-                var correct = answerArray.indexOf(userChoice);
-                censoredWord[correct] = chosenWord[correct];
-            }
         }
     }
 }
