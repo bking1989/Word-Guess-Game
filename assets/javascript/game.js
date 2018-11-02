3bfd83cf2bd7213b4c03254a28bddbe36cc873fd
bking1989 <bradley_king@me.com>
2018-11-01 19:35:54 -0700

    Changed up HTML format and stylesheet, totally redid the JS script so that it works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,106 @@
-// First, we're globally defining our HTML elements via CSS class
+// Global defined variables start here
 
-var opener = document.getElementById("openingText");
+// The opening text
+var opener = document.getElementById("opener");
+
+// Define the unorganized list that will house all of our letters
 var gameBoard = document.getElementById("gameBoard");
-var winCount = document.getElementById("winTotal");
-var guessCount = document.getElementById("remainingGuesses");
-var incorrectAnswers = document.getElementById("wrongGuesses");
-var letter;
-var correctAnswers = ["n","i","r","v","a","n","a"];
-var wrong = document.getElementById("wrongGuesses");
-var newValue = document.createTextNode(" " + letter);
 
-// If the opener has not been cleared, the first keystroke does that
+// List of possible word choices
+var cities = ["Chicago","Baltimore","Austin","Phoenix","Portland"];
 
-if (opener.classList.contains("hide") == false) {
-    document.onkeyup = function(e) {
-        opener.classList.add("hide");
-        gameBoard.classList.remove("hide");
-        winCount.classList.remove("hide");
-        guessCount.classList.remove("hide");
-        incorrectAnswers.classList.remove("hide");
-    }
+// Random city chosen via random number generator
+var chosenCity = cities[Math.ceil(Math.random() * 4)];
 
-    // Once the opening is cleared, a keystroke starts the game
+// Answer key to compare user input to
+var answerKey = chosenCity.toLocaleUpperCase().split("");
 
-} else if (opener.classList.contains("hide")) {
-    document.onkeyup = function(e) {
-        letter = event.key.toLowerCase();
+// The chosen word is broken up into individual letters as HTML list items
+for (var i = 0; i < answerKey.length; i++) {
+    var letter = answerKey[i];
+    var listItem = document.createElement("li");
+    listItem.innerHTML = letter;
+    listItem.classList.add("blackout");
+    gameBoard.append(listItem);
+};
 
-        if (letter === "n") {
-            
-        }
-    }
-}
+// Set up the game interface
+
+// Wins section
+var totalWins = document.getElementById("totalWins");
+var winCounter = 0;
+totalWins.innerHTML = "Wins: " + winCounter;
+
+// Losses section
+var totalLosses = document.getElementById("totalLosses");
+var lossCounter = 0;
+totalLosses.innerHTML = "Losses: " + lossCounter;
+
+// Guesses Left section
+var totalGuesses = document.getElementById("totalGuesses");
+var rmGuesses = 20;
+totalGuesses.innerHTML = "Guesses Remaining: " + rmGuesses;
+
+// Guesses Made section
+var guessesMade = document.getElementById("guessesMade");
+var guessCounter = [];
+guessesMade.innerHTML = "Guesses Made: " + guessCounter;
+
+// The game starts here
+
+window.onload = function() {
+    // The game starts by the user pressing any key
+    document.onkeyup = function(event) {
+        // If the opener hasn't been cleared, then the first keypress reveals the game interface
+        if (opener.classList.contains("hide") == false) {
+            opener.classList.add("hide");
+            gameBoard.classList.remove("hide");
+            totalWins.classList.remove("hide");
+            totalLosses.classList.remove("hide");
+            totalGuesses.classList.remove("hide");
+            guessesMade.classList.remove("hide");
+        // If the opener has already been cleared, then the keypress is registered as a variable, and the game runs its course
+        } else if (opener.classList.contains("hide") && rmGuesses >= 1) {
+            var userInput = event.key.toLocaleUpperCase();
+            // This will check to see if any part of the word is unsolved. If there are none, then the player wins, and can reset the puzzle
+            if (gameBoard.getElementsByClassName("blackout").length == 0) {
+                alert("You have solved the word puzzle! Congratulations! Play again?");
+                gameBoard.innerHTML = "";
+                chosenCity = cities[Math.ceil(Math.random() * 4)];
+                answerKey = chosenCity.toLocaleUpperCase().split("");
+                for (var i = 0; i < answerKey.length; i++) {
+                    var letter = answerKey[i];
+                    var listItem = document.createElement("li");
+                    listItem.innerHTML = letter;
+                    listItem.classList.add("blackout");
+                    gameBoard.append(listItem);
+                };
+                rmGuesses = 20;
+                totalGuesses.innerHTML = "Guesses Remaining: " + rmGuesses;
+                guessCounter = [];
+                guessesMade.innerHTML = "Guesses Made: " + guessCounter;
+            // If the user's answer doesn't match anything in the array, then they lose a guess and the answer is recorded
+            } else if (answerKey.indexOf(userInput) == -1 && gameBoard.getElementsByClassName("blackout").length >= 1) {
+                lossCounter++;
+                totalLosses.innerHTML = "Losses: " + lossCounter;
+                rmGuesses--;
+                totalGuesses.innerHTML = "Guesses Remaining: " + rmGuesses;
+                guessCounter.push(userInput + " ");
+                guessesMade.innerHTML = "Guesses Made: " + guessCounter.join("");
+            // If the user's answer matches an entry in the array, then they don't lose a guess and can continue
+            } else if (answerKey.indexOf(userInput) >= 0) {
+                var value = answerKey.indexOf(userInput);
+                gameBoard.children[value].classList.remove("blackout");
+                winCounter++;
+                totalWins.innerHTML = "Wins: " + winCounter;
+                rmGuesses;
+                totalGuesses.innerHTML = "Guesses Remaining: " + rmGuesses;
+                guessCounter.push(userInput + " ");
+                guessesMade.innerHTML = "Guesses Made: " + guessCounter.join("");
+            };
+        // If you run out of remaining guesses, no more entries are logged, and it's game over with no restart
+        } else if (opener.classList.contains("hide") && rmGuesses <= 0) {
+            alert("You ran out of tries! Game over!")
+        };  
+    };
+};
--- assets/javascript/randomWordSequence.js
+++
@@ -1,6 +0,0 @@
-// First, our game will select a word to use
-
-var possibleWords = ["Come As You Are","Black Hole Sun","Interstate Love Song","Killing in the Name","Scar Tissue"];
-var chosenWord = possibleWords[Math.floor(Math.random() * 5)]; //Randomly picks one of the words
-var answerArray = chosenWord.split(); //Splits the selected word into an array
-var censoredWord = chosenWord.replace(/[a-z]/gi,"_"); //Replaces all letters in the string with an underscore
